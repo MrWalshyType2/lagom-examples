0795ee5882ad4efd17b005e81188aa125043333b
MrWalshyType2 <mwalsh@academytrainee.com>
2020-10-08 10:54:55 +0100

    Added implementation of test()

--- lagom-java-sbt/lagom-java-sbt/product-impl/src/main/java/com/example/inventory/impl/ProductServiceImpl.java
+++ lagom-java-sbt/lagom-java-sbt/product-impl/src/main/java/com/example/inventory/impl/ProductServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.inventory.impl;
 
-public class ProductServiceImpl {
+import akka.NotUsed;
+import com.example.inventory.api.ProductService;
+import com.lightbend.lagom.javadsl.api.Descriptor;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public ServiceCall<NotUsed, String> test() {
+        // takes an empty request body, returns a String
+        return notUsed -> {
+            return supplyAsync(() -> {
+                return "Product service is up";
+            });
+        };
+    }
+
+    @Override
+    public Descriptor descriptor() {
+        return null;
+    }
 }
